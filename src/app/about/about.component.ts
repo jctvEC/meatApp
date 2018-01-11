18c913d33855eee7942c4b66b76931115c5031b9
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2018-01-11 12:01:12 -0300

    Testando Implementação de de lib JS no projeto
    Testando lodash, atencao ao uso da npm install --save lib@versao e o npm install --save-dev lib@versao, uma é para runtime, tem de execucao real da aplicacao e outra é pra ajudar no desenvolvimento.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash'
+
+console.log(_.pad("TesteDeImportacaoDeBibliotecaJavaScript", 60, "=")) //olhar no console
+//video aula secao 3 aula 15.
 
 @Component({
   selector: 'mt-about',
